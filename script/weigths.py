c5fa500c1508f36de56f72e8de5fb6034e7a3303
michelemorello97 <42273784+michelemorello97@users.noreply.github.com>
2023-09-03 17:07:27 +0200

    Caricamento file esame

---
+++ script/weigths.py
@@ -0,0 +1,40 @@
+from itertools import repeat
+import pandas
+
+df = pandas.read_csv('AppCat2.csv')
+
+print('\nPreview:')
+print(df)
+
+print('\nNumber of rows: ' + str(df.shape[0]))
+
+weights = []
+
+count = 0
+actualID = df['appid'][0]
+
+for i in range(df.shape[0]):
+
+    if df['appid'][i] == actualID:
+        count += 1
+
+    else:
+        weights.extend((1/count) for i in range(count))
+
+        count = 1
+        actualID = df['appid'][i]
+
+
+    # To not skip the last weight addition of the last game. 
+    if i == df.shape[0] - 1: 
+        weights.extend((1/count) for i in range(count))
+
+
+dfWithWeights = df.assign(weights=weights)
+
+print('\Dataframe preview with weights:')
+print(dfWithWeights)
+
+dfWithWeights.to_csv('AppCat2_weights.csv', index=False)
+
+# Uncomment to save the new dataframes on a csv dfs.
